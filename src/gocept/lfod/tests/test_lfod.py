7cee141961752d13cc44627e8658c66dc31fa06a
Sebastian Wehrmann <sw@gocept.com>
2012-01-19 09:22:10 +0100

    init project

---
+++ src/gocept/lfod/tests/test_lfod.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import os
+import gocept.exttest
+import pkg_resources
+
+
+def test_suite():
+    return gocept.exttest.makeSuite(
+        os.environ.get('jasmine-bin'),
+        '--json',
+        pkg_resources.resource_filename('gocept.lfod', 'js/tests'))
