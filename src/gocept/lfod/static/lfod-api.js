40a8acfc3149792692beb9a8a880255d21a21d44
Daniel Havlik <dh@gocept.com>
2012-01-19 11:16:15 +0100

    add api

---
+++ src/gocept/lfod/static/lfod-api.js
@@ -0,0 +1,31 @@
+window['lfod'] = {} //namespace declaration
+
+(function() {
+    lfod.Lfod = function() {
+        this.construct.apply(this, arguments);
+    }
+
+    lfod.Lfod.prototype = {
+        construct: function(database_url) {
+            this.database_url = database_url;
+        },
+        fetch: function(fetcher, eaters, guests, callback) {
+            callback();
+        },
+        get_ranking: function(callback) {
+            //get lfodders sorted by score and pass to callback
+            callback({});
+        },
+        get_fetchers: function(callback) {
+            //get lfodders sorted by name and pass to callback
+            callback({});
+        }
+    }
+
+})();
+
+
+
+//api.data = [{'id': 'nilo', 'name': 'Nilo', 'score': '5', 'avatar_url': ''},
+//            {'id': 'basti', 'name': 'Basti', 'score': '3', 'avatar_url': ''}];
+
