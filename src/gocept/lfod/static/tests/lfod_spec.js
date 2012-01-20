0c26670e1f9b96372803652e0cc2d615d19127c4
Sebastian Wehrmann <sw@gocept.com>
2012-01-20 14:24:29 +0100

    repair tests.

--- src/gocept/lfod/static/tests/lfod_spec.js
+++ src/gocept/lfod/static/tests/lfod_spec.js
@@ -77,6 +77,7 @@ describe('Lfod can handle guests', function() {
     var callback = function() {};
     var set_score_mock = spyOn(mylfod, 'db_set_score');
     var current_score = {'nilo': -3, 'basti': 1, 'zagy': 2, 'guests': 0};
+    spyOn(mylfod, 'db_log_fetch');
     spyOn(mylfod, 'db_get_score').andCallFake(
         function(fetcher_id) {return current_score[fetcher_id]});
     mylfod.fetch('nilo', ['basti', 'zagy'], 5, callback);
@@ -108,7 +109,30 @@ describe('Handling of database JSON results', function() {
                   {"id": "nilo",
                    "key": "Nilo",
                    "value": {"name":"Nilo", "score":4}}]})
-    expect(mylfod.list_fetchers('name')).toEqual(
-      [{name: 'Basti', score: 0, id: 'basti'},
-       {name: 'Nilo', score: 4, id: 'nilo'}]);
+    it('list_fetchers returns name, score and id of fetchers', function() {
+        expect(mylfod.list_fetchers('name')).toEqual(
+          [{name: 'Basti', score: 0, id: 'basti'},
+           {name: 'Nilo', score: 4, id: 'nilo'}]);
+    });
+});
+
+describe('Calculation of the logs', function() {
+    spyOn(mylfod, 'db_get_last_fetches').andReturn(
+        [{'fetcher': 'andrea', time: 1327064136876},
+         {'fetcher': 'nilo', time: 1327063813836},
+         {'fetcher': 'basti', time: 1327063778076}])
+    fetcher_names = {'andrea': {'name': 'Andrea'},
+                     'basti': {'name': 'Basti'},
+                     'nilo': {'name': 'Nilo'}};
+    spyOn(mylfod, 'db_get_fetcher').andCallFake(
+        function(fetcher_id) {return fetcher_names[fetcher_id]});
+    spyOn(mylfod, 'db_log_fetch');
+    var callback = jasmine.createSpy('callback');
+    mylfod.get_last_fetches(callback);
+    it('callback is called with fetcher and date logs', function() {
+        expect(callback).toHaveBeenCalledWith(
+          [{date: 'January 20, 2012 13:55', fetcher: 'Andrea'},
+           {date: 'January 20, 2012 13:50', fetcher: 'Nilo'},
+           {date: 'January 20, 2012 13:49', fetcher: 'Basti'}]);
+    });
 });
